25e27f6f3117f486c41c74f2f8d4c9933779405b
MoizSiddique <moizsiddique27@gmail.com>
2022-12-27 18:15:32 +0500

    Game Completed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+#!/usr/bin/env node
 import inquirer from "inquirer";
 import chalk from 'chalk';
+import chalkAnimation from "chalk-animation";
 
 const error = chalk.bgRed;
 const warning = chalk.bgYellow;
-const success = chalk.bgGreenBright;
-const fail = chalk.bgRedBright;
+const heading = chalk.cyan;
 
 const min : number = 1;
 const max : number = 10;
@@ -18,6 +19,11 @@ let pause = (time : number)=>{
 
 let startFlag : boolean = true;
 let attempt : number = 3; 
+const intro = heading(`\n Welcome to the Number Guessing Game!\n In this game, you will try to guess a randomly chosen number between ${min} and ${max}.\n You will have ${attempt} chances to guess the correct number. Good luck!`);
+
+console.log(intro);
+await pause(1000);
+
 while(startFlag && (attempt > 0)){
 
     let userInput = await inquirer.prompt([{
@@ -28,25 +34,42 @@ while(startFlag && (attempt > 0)){
 
     if((userInput.field < min) || (userInput.field > max)){
         console.log(error("Your input value is out of range please enter valid number..."));
-        await pause(700);
+        await pause(1200);
     }else if(isNaN(userInput.field) || ((userInput.field % 1) != 0)){
         console.log(error("Invalid value please enter valid number..."));
-        await pause(700);
+        await pause(1200);
     }else{
         if((attempt < 2) && (random !== userInput.field)){
-            console.log(fail("Ohh! You loose, your attempt's finished...."));
-            await pause(1000);
+            const loose = chalkAnimation.neon(`
+\\    / / //   ) ) //   / /       / /        //   ) ) //   ) ) //   ) )  //   / / 
+ \\  / / //   / / //   / /       / /        //   / / //   / / ((        //____    
+  \\/ / //   / / //   / /       / /        //   / / //   / /    \\      / ____     
+  / / //   / / //   / /       / /        //   / / //   / /       ) ) //          
+ / / ((___/ / ((___/ /       / /____/ / ((___/ / ((___/ / ((___ / / //____/ /  
+            `)
+            await pause(5000);
+            loose.stop();
         }else{
             if(random === userInput.field){
-                console.log(success("Hurry! You win..."));
+                const win = chalkAnimation.neon(`
+                                                           ___   ___
+                \\    / / //   ) ) //   / /   ||   / |  / /   / /    /|    / / 
+                 \\  / / //   / / //   / /    ||  /  | / /   / /    //|   / /  
+                  \\/ / //   / / //   / /     || / /||/ /   / /    // |  / /   
+                  / / //   / / //   / /      ||/ / |  /   / /    //  | / /    
+                 / / ((___/ / ((___/ /       |  /  | / __/ /___ //   |/ /  
+                `)
+                await pause(5000);
+                win.stop();
+                // console.log(success("Hurry! You win..."));
                 startFlag = false;
             }else if(userInput.field > random){
                 console.log(warning("Your are guesing high..."));
-                pause(500);
+                pause(1000);
                 
             }else{
                 console.log(warning("Your are guesing low..."));
-                pause(500);
+                pause(1000);
             }
         }
         attempt--;
